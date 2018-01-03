17a7358b91eeb2d1183fd8d3d45f8abbdaa7551c
chuckhnnyln <chuckh@nnyln.org>
2018-01-03 08:26:43 -0500

    Tweaks to shell scripts
    Specified local paths for execution from root cron job.

--- full_import.sh
+++ full_import.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 # This file runs all of the scripts required to pull from ContentDM and update the NYH exist database
+LOCALPATH="/home/ubuntu/nyh_scripts/Tealcat"
 
 echo Starting job
 echo Harvesting OAI from ContentDM
 # Remove the existing output.xml. This aids in failure checking
-rm output.xml
-python ./oaiharvest.py
+rm $LOCALPATH/output.xml
+echo Deleted previous output
+python $LOCALPATH/oaiharvest.py
 echo Finished OAI harvest
 
 echo Processing XML for collections
-./process_oai.sh
+$LOCALPATH/process_oai.sh
 
 echo Ending job
+
+exit 0
--- process_oai.sh
+++ process_oai.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 # This script processes the output of the oai download and uploads the XMl to exist
+LOCALPATH="/home/ubuntu/nyh_scripts/Tealcat"
 
 echo Starting process_oai
 
 echo Processing XML for collections
 # Remove existing local xml files
-rm /home/ubuntu/output/*.xml
-python /home/ubuntu/parseOAItoXML.py
+rm $LOCALPATH/output/*.xml
+python $LOCALPATH/parseOAItoXML.py
 echo Finished processing XML
 
 echo Uploading processed files to eXistDB
-python /home/ubuntu/output/uploadXML.py
+python $LOCALPATH/uploadXML.py
 echo Finished uploading
 
 echo Ending process_oai
+
+exit 0
